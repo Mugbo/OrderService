f53567098cb324c1f49c90500146c96978776b13
Michael Agbo <Michael.Agbo@genesys.com>
2024-01-10 20:09:19 +0000

    amended OrderController OrderDetails and OrderService and set port to 8081

--- src/main/java/atu/orderservive/OrderController.java
+++ src/main/java/atu/orderservive/OrderController.java
@@ -1,5 +1,23 @@
 package atu.orderservive;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class OrderController {
+
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping("/confirmOrder")
+    public String confirmOrder(@RequestBody OrderDetails orderDetails){
+        return orderService.confirmOrder(orderDetails);
+    }
 }
--- src/main/java/atu/orderservive/OrderService.java
+++ src/main/java/atu/orderservive/OrderService.java
@@ -4,4 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
+    public String confirmOrder(OrderDetails orderDetails){
+        return String.format("Received Order for %.2f Euro", orderDetails.getCost());
+    }
 }
